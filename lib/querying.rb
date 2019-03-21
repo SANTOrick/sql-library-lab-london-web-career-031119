92558bd911cf008bd2210722a833a4484041ce22
Riccardo <riccardo_london@live.it>
2019-03-21 14:17:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT subgenres.name, subgenres.author "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title JOIN characters ON characters.series_id = series.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
