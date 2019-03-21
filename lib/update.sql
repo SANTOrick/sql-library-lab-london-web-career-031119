9fbb690ea9c9b478d447f7f267f4b17ae185a725
Riccardo <riccardo_london@live.it>
2019-03-21 13:53:39 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters SET species = "Martian" WHERE id = (select * from (select max(id) from imagedata) as t)
+UPDATE characters SET species = "Martian" WHERE id = (select * from (select max(id) from characters) as t)
